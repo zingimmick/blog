1e267596a294f85198ab374085c7e7f2278154e2
zingimmick <zingimmick@outlook.com>
2022-02-24 19:27:08 +0800

    giscus

--- src/components/comments.js
+++ src/components/comments.js
@@ -7,12 +7,19 @@ export default class Comments extends Component {
   }
   componentDidMount () {
       let scriptEl = document.createElement("script");
-      scriptEl.setAttribute("src", "https://utteranc.es/client.js");
+      scriptEl.setAttribute("src", "https://giscus.app/client.js");
       scriptEl.setAttribute("crossorigin","anonymous");
       scriptEl.setAttribute("async", true);
-      scriptEl.setAttribute("repo", "zingimmick/blog");
-      scriptEl.setAttribute("issue-term", "title");
-      scriptEl.setAttribute( "theme", "github-light");
+      scriptEl.setAttribute("data-repo", "zingimmick/blog");
+      scriptEl.setAttribute("data-repo-id","R_kgDOG5nuog");
+      scriptEl.setAttribute("data-category","Announcements");
+      scriptEl.setAttribute("data-category-id","DIC_kwDOG5nuos4CBUHZ");
+      scriptEl.setAttribute("data-mapping","title");
+      scriptEl.setAttribute("data-reactions-enabled","1");
+      scriptEl.setAttribute("data-emit-metadata","0");
+      scriptEl.setAttribute("data-input-position","bottom");
+      scriptEl.setAttribute("data-theme","preferred_color_scheme");
+      scriptEl.setAttribute("data-lang","zh-CN");
       this.commentBox.current.appendChild(scriptEl);
   }
 
