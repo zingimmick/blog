a1ead1e12a05899ca4c680a48542e99943962ad9
zingimmick <zingimmick@outlook.com>
2022-02-24 19:12:45 +0800

    Add imports

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Comments from "../components/comments"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
