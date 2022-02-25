e38e4b9d04a5808434edebda728de39aa6ad3d7c
zingimmick <zingimmick@outlook.com>
2022-02-25 12:11:46 +0800

    Comments powered by Giscus(A comments system powered by GitHub Discussions) (#1)

---
+++ src/components/comments.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+export default class Comments extends Component {
+  constructor(props){ 
+    super(props);
+    this.commentBox = React.createRef(); 
+  }
+  componentDidMount () {
+      let scriptEl = document.createElement("script");
+      scriptEl.setAttribute("src", "https://giscus.app/client.js");
+      scriptEl.setAttribute("crossorigin","anonymous");
+      scriptEl.setAttribute("async", true);
+      scriptEl.setAttribute("data-repo", "zingimmick/blog");
+      scriptEl.setAttribute("data-repo-id","R_kgDOG5nuog");
+      scriptEl.setAttribute("data-category","Announcements");
+      scriptEl.setAttribute("data-category-id","DIC_kwDOG5nuos4CBUHZ");
+      scriptEl.setAttribute("data-mapping","title");
+      scriptEl.setAttribute("data-reactions-enabled","1");
+      scriptEl.setAttribute("data-emit-metadata","0");
+      scriptEl.setAttribute("data-input-position","bottom");
+      scriptEl.setAttribute("data-theme","light");
+      scriptEl.setAttribute("data-lang","zh-CN");
+      this.commentBox.current.appendChild(scriptEl);
+  }
+
+  render() {
+    return (
+        <div>
+          <div ref={this.commentBox} className="comment-box"/>
+        </div>
+    );
+  }
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Comments from "../components/comments"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -34,6 +35,7 @@ const BlogPostTemplate = ({ data, location }) => {
           <Bio />
         </footer>
       </article>
+      <Comments/>
       <nav className="blog-post-nav">
         <ul
           style={{
